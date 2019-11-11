07dcbe9b9a4e32d991a3d7a30533b7b141df3787
Dovahkid <gainmikey@gmail.com>
2019-11-10 21:10:28 -0500

    1.1.0
    Updated for flexibility and easier updates in the future

--- vignere library/vigenere_lib.cs
+++ vignere library/vigenere_lib.cs
@@ -8,11 +8,17 @@ namespace vigenere_library
 {
     static public class vig
     {
-        public static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // I know this is basically unnecesary as it is right now. This is done for later update ability
+        public static string alphabet
+        {
+            get { return _alphabet; }
+            set { _alphabet = value; }
+        }
 
         static public string encrypt(string plainText, string key)
         {
-            
+
             int temp;
             int overFlow;
             string vigOutput = "";
@@ -23,9 +29,9 @@ namespace vigenere_library
             for (int i = 0; i <= plainText.Length - 1; i++)
             {
                 temp = alphabet.IndexOf(key[i % key.Length]) + alphabet.IndexOf(plainText[i]);
-                if (temp >= 26)
+                if (temp >= alphabet.Length)
                 {
-                    overFlow = temp - 26;
+                    overFlow = temp - alphabet.Length;
                     vigOutput += alphabet[overFlow];
                 }
                 else
@@ -50,14 +56,14 @@ namespace vigenere_library
             for (int i = 0; i <= plainText.Length - 1; i++)
             {
                 temp = alphabet.IndexOf(plainText[i]) - alphabet.IndexOf(key[i % key.Length]);
-                if (temp >= 26)
+                if (temp >= alphabet.Length)
                 {
-                    overFlow = temp - 26;
+                    overFlow = temp - alphabet.Length;
                     vigOutput += alphabet[overFlow];
                 }
                 else if (temp < 0)
                 {
-                    overFlow = 26 + temp;
+                    overFlow = alphabet.Length + temp;
                     vigOutput += alphabet[overFlow];
                 }
                 else
